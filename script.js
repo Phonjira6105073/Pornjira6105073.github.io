91a49b4ad63b40990de7bcff14b2120167bbd0fb
Phonjira <84186416+Pornjira6105073@users.noreply.github.com>
2022-04-10 10:34:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function getData() {
+
+    const username = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+
+    if (username == "" | password == "") {
+        alert("Please enter your Username and Password.");
+    }
+    else {
+        localStorage.setItem("sName",username);
+        localStorage.setItem("sPass",password);
+
+        window.location="page2.html";
+    }
+}
